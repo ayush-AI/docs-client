5c79d23ded14097818f9a53f19a5745f065f9dd2
Ayush <ayushmishra2854@gmail.com>
2023-10-03 20:26:43 +0530

    added the navbar'

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,82 +1,96 @@
-import { useEffect, useState, useRef } from 'react';
-import ReactQuill from 'react-quill';
-import { DeltaStatic } from 'quill';
-import { modules, formats } from '../utils/QuillConfig';
-import 'react-quill/dist/quill.snow.css';
-import { Box } from '@mui/material';
-import { io, Socket } from 'socket.io-client';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState, useRef } from "react";
+import ReactQuill from "react-quill";
+import { DeltaStatic } from "quill";
+import { modules, formats } from "../utils/QuillConfig";
+import "react-quill/dist/quill.snow.css";
+import { Box } from "@mui/material";
+import { io, Socket } from "socket.io-client";
+import { useParams } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function Editor() {
-    const [value, setValue] = useState('');
-    const [socket, setSocket] = useState<Socket>();
-    const quillRef = useRef<ReactQuill>(null);
-    const { id: documentId } = useParams();
-    
-    useEffect(() => {
-        const s = io(import.meta.env.VITE_API_URL as string || 'http://localhost:5000', { transports: ['websocket'] });
-        setSocket(s);
-        return () => {
-            s.disconnect();
-        }
-    },[]);
+  const [value, setValue] = useState("");
+  const [socket, setSocket] = useState<Socket>();
+  const quillRef = useRef<ReactQuill>(null);
+  const { id: documentId } = useParams();
 
-    useEffect(() => { 
-        quillRef.current?.getEditor().disable();
-        quillRef.current?.getEditor().setText('Loading...');
-    },[]);
+  useEffect(() => {
+    const s = io(
+      (import.meta.env.VITE_API_URL as string) || "http://localhost:5000",
+      { transports: ["websocket"] }
+    );
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
 
-    useEffect(() => {
-        if(socket == null || quillRef == null) return;
-        socket.once('load-document', document => {
-            quillRef.current?.getEditor().setContents(document);
-            quillRef.current?.getEditor().enable();
-        });
-        socket.emit('get-document', documentId);
-    },[socket, quillRef, documentId]);
+  useEffect(() => {
+    quillRef.current?.getEditor().disable();
+    quillRef.current?.getEditor().setText("Loading...");
+  }, []);
 
-    useEffect(() => {
-        if(socket == null || quillRef == null) return;
+  useEffect(() => {
+    if (socket == null || quillRef == null) return;
+    socket.once("load-document", (document) => {
+      console.log(document);
+      quillRef.current?.getEditor().setContents(document);
+      quillRef.current?.getEditor().enable();
+    });
+    socket.emit("get-document", documentId);
+  }, [socket, quillRef, documentId]);
 
-        const handler = (delta: unknown, _oldDelta: unknown, source: unknown) => {
-            if(source !== 'user') return;
-            socket.emit('send-changes', delta);
-        }
-        quillRef.current?.getEditor().on('text-change', handler);
-        return () => {
-            quillRef.current?.getEditor().off('text-change', handler);
-        }
-    },[socket, quillRef]);
+  useEffect(() => {
+    if (socket == null || quillRef == null) return;
 
-    useEffect(() => {
-        if(socket == null || quillRef == null) return;
-        
-        const handler = (delta: DeltaStatic) => {
-            quillRef.current?.getEditor().updateContents(delta);
-        }
-        socket.on('receive-changes', handler);
-        return () => {
-            socket.off('receive-changes', handler);
-        }
-    },[socket, quillRef]);
+    const handler = (delta: unknown, _oldDelta: unknown, source: unknown) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quillRef.current?.getEditor().on("text-change", handler);
+    return () => {
+      quillRef.current?.getEditor().off("text-change", handler);
+    };
+  }, [socket, quillRef]);
 
-    useEffect(() => {
-        if(socket == null || quillRef == null) return;
-        
-        const interval = setInterval(() => {
-            socket.emit('save-document', quillRef.current?.getEditor().getContents());
-        }, 2000);
-        return () => {
-            clearInterval(interval);
-        }
-    },[socket, quillRef]);
-    
-    
+  useEffect(() => {
+    if (socket == null || quillRef == null) return;
+
+    const handler = (delta: DeltaStatic) => {
+      quillRef.current?.getEditor().updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quillRef]);
+
+  useEffect(() => {
+    if (socket == null || quillRef == null) return;
+
+    const interval = setInterval(() => {
+      socket.emit("save-document", quillRef.current?.getEditor().getContents());
+    }, 2000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [socket, quillRef]);
+
   return (
-    <Box sx={{ height: '100vh', width: '100%', backgroundColor: '#f5f5f5' }}>
-    <ReactQuill theme="snow" value={value} onChange={setValue} modules={modules} formats={formats} ref={quillRef}/>
-    </Box>
-  )
+    <>
+      <Box sx={{ height: "100vh", width: "100%", backgroundColor: "#f5f5f5" }}>
+        <Navbar />
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          modules={modules}
+          formats={formats}
+          ref={quillRef}
+        />
+      </Box>
+    </>
+  );
 }
 
-export default Editor
+export default Editor;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+
+function Navbar() {
+  return (
+    <>
+      <AppBar
+        position="sticky"
+        sx={{
+          background: "white",
+          height: "6vh",
+          boxShadow: "none",
+          display: {
+            xs: "flex",
+            justifyContent: "center",
+          },
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Box sx={{ flexGrow: 1 }}>
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="#app-bar-with-responsive-menu"
+                sx={{
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "#000",
+                  textDecoration: "none",
+                }}
+              >
+                DOCS
+              </Typography>
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Button variant="contained">Export as PDF</Button>
+            </Box>
+          </Toolbar>
+          {/* <Button>Export as PDF</Button> */}
+        </Container>
+      </AppBar>
+    </>
+  );
+}
+export default Navbar;
